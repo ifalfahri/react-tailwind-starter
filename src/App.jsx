732de920f94baaec9e49978ee379cf29e5a9fc96
IfalEXZ <ifaltampan69@gmail.com>
2024-11-22 13:51:31 +0800

    fix: update links in the App component for React, TailwindCSS, and Vite

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 function App() {
-  return (
+  return ( // you can delete it and replace with your own code
     <main className="flex flex-col justify-center items-center min-h-screen space-y-10"> 
       <h1 className="text-5xl font-extrabold text-center">
-        <a href="" className="text-[#61dbfb] hover:text-[#56c8e5] transition-colors">
+        <a href="https://react.dev/" className="text-[#61dbfb] hover:text-[#56c8e5] transition-colors">
           React
-        </a> + <a href="" className="text-sky-400 hover:text-sky-500 transition-colors">
+        </a> + <a href="https://tailwindcss.com/" className="text-sky-400 hover:text-sky-500 transition-colors">
           TailwindCSS
-        </a> + <a href="" className="text-violet-500 hover:text-violet-600 transition-colors">
+        </a> + <a href="https://vite.dev/" className="text-violet-500 hover:text-violet-600 transition-colors">
           Vite
         </a> starter
       </h1>
