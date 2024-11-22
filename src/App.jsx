28dfdd292df7db5782db8e45183bccf94110eee7
IfalEXZ <ifaltampan69@gmail.com>
2024-11-22 11:52:23 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+function App() {
+  return (
+    <main className="flex flex-col justify-center items-center min-h-screen space-y-10">
+      <h1 className="text-5xl font-extrabold">
+        React + TailwindCSS + Vite starter
+      </h1>
+      <a
+        href="https://github.com/ifalfahri/react-tailwind-starter"
+        target="_blank"
+        className="bg-gray-900 text-white py-2 px-4 rounded-lg hover:bg-black transition-colors"
+      >
+        ⭐️ on GitHub
+      </a>
+    </main>
+  );
+}
+
+export default App;
